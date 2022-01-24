1d98398146985f59b498c2d51a0dcc19d10bea1c
adrian.nidecki <adrian.nidecki@accenture.com>
2022-01-24 17:34:03 +0100

    debug to fix production

--- utils/apolloClient.ts
+++ utils/apolloClient.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+console.log(process.env.NODE_ENV);
+console.log(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`);
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+      : `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
   cache: new InMemoryCache(),
 });
 
