ec1cd53d2bd40441cd540e3e3db33da805333fd8
adrian.nidecki <adrian.nidecki@accenture.com>
2022-01-24 17:18:45 +0100

    change uri apollo

--- utils/apolloClient.ts
+++ utils/apolloClient.ts
@@ -1,10 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
-      : `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
   cache: new InMemoryCache(),
 });
 
