f7918d810e4e5b6eaae96220c58470feb8fb3a1e
adrian.nidecki <adrian.nidecki@accenture.com>
2022-01-24 17:26:44 +0100

    remove getStaticProps to debug on prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import client from "../utils/apolloClient";
-import { GetProductsDocument, GetProductsQuery } from "../generated/graphql";
-import { GetStaticProps, NextPage } from "next";
+import { GetProductsQuery } from "../generated/graphql";
+import { NextPage } from "next";
 
 interface HomeProps {
   getProducts: GetProductsQuery;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await client.query({
-    query: GetProductsDocument,
-  });
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await client.query({
+//     query: GetProductsDocument,
+//   });
 
-  return {
-    props: {
-      getProducts: res.data,
-    },
-  };
-};
+//   return {
+//     props: {
+//       getProducts: res.data,
+//     },
+//   };
+// };
 
 const Home: NextPage<HomeProps> = ({ getProducts }) => {
+  console.log(getProducts);
   return (
     <Container maxWidth="lg">
       <Box
@@ -36,9 +36,6 @@ const Home: NextPage<HomeProps> = ({ getProducts }) => {
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
-        {getProducts.products.map((product) => (
-          <Typography key={product!.id}>{product!.name}</Typography>
-        ))}
       </Box>
     </Container>
   );
