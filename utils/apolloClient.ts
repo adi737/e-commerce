61e0f7a036f08e3c7db49316470e5e195fbd3e46
adrian.nidecki <adrian.nidecki@accenture.com>
2022-01-24 17:24:24 +0100

    fix bugs to deploy the app

--- utils/apolloClient.ts
+++ utils/apolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-
+console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
 const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
   cache: new InMemoryCache(),
