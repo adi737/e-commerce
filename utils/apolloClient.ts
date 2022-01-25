4214015a363864e4e77f4026ffaf20caee3dc93b
adrian.nidecki <adrian.nidecki@accenture.com>
2022-01-25 10:48:07 +0100

    debug prod

--- utils/apolloClient.ts
+++ utils/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === "production"
-      ? "https://e-commerce.vercel.app/api/graphql"
+      ? "https://e-commerce-adi737.vercel.app/api/graphql"
       : "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
